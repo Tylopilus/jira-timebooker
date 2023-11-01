75aed606a99e1831a32a81abbe95726b3df9c0c7
Helge <elitaer@gmail.com>
2023-11-01 20:30:10 +0100

    New Version

--- src/manifest.ts
+++ src/manifest.ts
@@ -3,7 +3,7 @@ import { defineManifest } from '@crxjs/vite-plugin';
 export default defineManifest({
    name: 'jira timebooker',
    description: 'Enables you to book time in your jira based on your meetings in outlook calendar',
-   version: '1.0.1',
+   version: '1.0.2',
    manifest_version: 3,
    icons: {
       '16': 'img/icon16.png',
